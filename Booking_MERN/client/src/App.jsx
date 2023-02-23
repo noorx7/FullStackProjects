1820c58f7d0caff1cf081017c778dc10a42260f8
noorx7 <nuruli@mun.ca>
2023-02-22 21:31:26 -0330

    RegisterBEStart

--- Booking_MERN/client/src/App.jsx
+++ Booking_MERN/client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Route path="/" element={<Layout />}>
 				<Route index element={<IndexPage />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
 			</Route>
 		</Routes>
 	);
--- Booking_MERN/client/src/pages/LoginPage.jsx
+++ Booking_MERN/client/src/pages/LoginPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function LoginPage() {
 	return (
 		<div className="mt-4 grow flex items-center justify-around">
@@ -11,6 +13,12 @@ function LoginPage() {
 					<input type="email" placeholder="your@email.com" />
 					<input type="password" placeholder="password" />
 					<button className="primary">Login</button>
+					<div className="text-center py-2 text-gray-500">
+						Don't have an account yet?{" "}
+						<Link className="underline text-black" to={"/register"}>
+							Register now
+						</Link>
+					</div>
 				</form>
 			</div>
 		</div>
