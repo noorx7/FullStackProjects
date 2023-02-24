c4704e4d857db63b63cf794b2ecb7d5ef03c5d1a
noorx7 <nuruli@mun.ca>
2023-02-23 23:23:56 -0330

    Register1

--- Booking_MERN/api/index.js
+++ Booking_MERN/api/index.js
@@ -1,13 +1,43 @@
+
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const User = require("./models/User")
+require("dotenv").config()
+app.use(express.json())
+const bcryptsalt = bcrypt.genSaltSync(10)
+
+
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }))
 
+
+mongoose.connect(process.env.MONGO_URL)
+
 app.get("/test", (req, res) => {
     res.json("test ok")
+})
+
+//fNzvzYqeegFzQaAY
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptsalt),
+        })
+        res.json(userDoc)
+    } catch (e) {
+        res.status(422).json(e)
+    }
+
+
 })
 
 app.listen(4000);
--- Booking_MERN/client/src/pages/RegisterPage.jsx
+++ Booking_MERN/client/src/pages/RegisterPage.jsx
@@ -7,9 +7,18 @@ function RegisterPage() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	function registerUser(ev) {
+	async function registerUser(ev) {
 		ev.preventDefault();
-		axios.get("http://localhost:4000/test");
+		try {
+			await axios.post("/register", {
+				name,
+				email,
+				password,
+			});
+			alert("Registration Successful. Now you can log in");
+		} catch (e) {
+            alert("Registration failed. Please try again later")
+        }
 	}
 
 	return (
